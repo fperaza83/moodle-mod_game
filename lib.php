4501a57bf6946d1ceb75e4dfd96f3519e7816a82
fperaza83 <fperaza@e-abclearning.com>
2024-03-18 10:41:45 -0400

    fix warning php 8 preg_replace param 3 null

--- lib.php
+++ lib.php
@@ -1245,7 +1245,12 @@ if (defined( 'GAME_MOODLE_401')) {
             return;
         }
         $type = new stdClass;
-        $type->name = preg_replace('/.*type=/', '', $type->type=(isset($type->type)? $type->type: null));
+        if ($type->type!==null){
+            $type->name = preg_replace('/.*type=/', '',$type->type);
+        }
+        else {
+            $type->name = '';
+        } 
         $type->title = get_string('pluginname', 'game').' - '.get_string('game_'.$kind, 'game');
         $type->link = new moodle_url('/course/modedit.php',
             ['add' => 'game', 'return' => 0, 'type' => $kind, 'course' => $course->id, 'id' => $course->id]);
